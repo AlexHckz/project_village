f56fea0d3e58307b81e86eda583fdcef428eccd6
SouadT <103109134+SouadT@users.noreply.github.com>
2022-06-09 12:23:50 +0200

    commit souad 1
    liens accueil

--- forum_utilisateur/components/footer.php
+++ forum_utilisateur/components/footer.php
@@ -5,7 +5,7 @@
             <div class="col-md-10 col-lg-8 col-xl-7">
                 <ul class="list-inline text-center">
                     <li class="list-inline-item">
-                        <a href="#!">
+                        <a href="https://www.google.com/search?client=avast-a-1&q=twitter&oq=twit&aqs=avast.1.69i57j0l7.5270j0j1&ie=UTF-8">
                             <span class="fa-stack fa-lg">
                                 <i class="fas fa-circle fa-stack-2x"></i>
                                 <i class="fab fa-twitter fa-stack-1x fa-inverse"></i>
@@ -13,7 +13,7 @@
                         </a>
                     </li>
                     <li class="list-inline-item">
-                        <a href="#!">
+                        <a href="https://www.facebook.com/">
                             <span class="fa-stack fa-lg">
                                 <i class="fas fa-circle fa-stack-2x"></i>
                                 <i class="fab fa-facebook-f fa-stack-1x fa-inverse"></i>
@@ -21,10 +21,26 @@
                         </a>
                     </li>
                     <li class="list-inline-item">
-                        <a href="#!">
+                        <a href="https://github.com/">
                             <span class="fa-stack fa-lg">
                                 <i class="fas fa-circle fa-stack-2x"></i>
                                 <i class="fab fa-github fa-stack-1x fa-inverse"></i>
+                            </span>
+                        </a>
+                    </li>
+                    <li class="list-inline-item">
+                        <a href="https://discordapp.com/channels/629205600412237826/974231519147229184">
+                            <span class="fa-stack fa-lg">
+                                <i class="fas fa-circle fa-stack-2x"></i>
+                                <i class="fab fa-discord fa-stack-1x fa-inverse"></i>
+                            </span>
+                        </a>
+                    </li>
+                    <li class="list-inline-item">
+                        <a href="https://www.angularjswiki.com/fr/fontawesome/">
+                            <span class="fa-stack fa-lg">
+                                <i class="fas fa-circle fa-stack-2x"></i>
+                                <i class="fab fa-trello fa-stack-1x fa-inverse"></i>
                             </span>
                         </a>
                     </li>

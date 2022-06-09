54415d441af2234090eea612efb57c03ebe23db5
SouadT <103109134+SouadT@users.noreply.github.com>
2022-06-09 12:28:59 +0200

    commit souad 1
    liens accueil

--- forum_utilisateur/components/footer.php
+++ forum_utilisateur/components/footer.php
@@ -4,22 +4,22 @@
         <div class="row gx-4 gx-lg-5 justify-content-center">
             <div class="col-md-10 col-lg-8 col-xl-7">
                 <ul class="list-inline text-center">
-                    <li class="list-inline-item">
+                    <!-- <li class="list-inline-item">
                         <a href="https://www.google.com/search?client=avast-a-1&q=twitter&oq=twit&aqs=avast.1.69i57j0l7.5270j0j1&ie=UTF-8">
                             <span class="fa-stack fa-lg">
                                 <i class="fas fa-circle fa-stack-2x"></i>
                                 <i class="fab fa-twitter fa-stack-1x fa-inverse"></i>
                             </span>
                         </a>
-                    </li>
-                    <li class="list-inline-item">
+                    </li> -->
+                    <!-- <li class="list-inline-item">
                         <a href="https://www.facebook.com/">
                             <span class="fa-stack fa-lg">
                                 <i class="fas fa-circle fa-stack-2x"></i>
                                 <i class="fab fa-facebook-f fa-stack-1x fa-inverse"></i>
                             </span>
                         </a>
-                    </li>
+                    </li> -->
                     <li class="list-inline-item">
                         <a href="https://github.com/">
                             <span class="fa-stack fa-lg">
